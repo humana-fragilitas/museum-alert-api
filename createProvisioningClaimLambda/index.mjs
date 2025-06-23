3767522b1e0f28d090a7d6a09ce41477efa7e957
Andrea Blasio <andrea@humanafragilitas.com>
2025-06-23 11:53:13 +0200

    Cleans up code and introduces standard error messages

--- createProvisioningClaimLambda/index.mjs
+++ createProvisioningClaimLambda/index.mjs
@@ -3,27 +3,21 @@ import {
   CreateProvisioningClaimCommand 
 } from "@aws-sdk/client-iot";
 
-/*
+import { 
+  errorApiResponse,
+  successApiResponse,
+  validateEnvironmentVariables
+} from '/opt/nodejs/shared/index.js'; 
 
-export enum AppErrorType {
-    UNAUTHORIZED,
-    FAILED_PROVISIONING_CLAIM_CREATION,
-    FAILED_EXISTING_THING_CHECK,
-    THING_ALREADY_EXISTS,
-    THING_ALREADY_EXISTS_IN_OTHER_ORGANIZATION,
-    GENERIC_ERROR
-};
 
-*/
+export const handler = async (event) => {
 
-export const handler = async (event, context, callback) => {
+  validateEnvironmentVariables([
+    'AWS_REGION'
+  ]);
   
+  const stage = event.requestContext?.stage;
   const region = process.env.AWS_REGION;
-  const identityPoolId = process.env.IDENTITY_POOL_ID;
-  const accountId = process.env.AWS_ACCOUNT_ID;
-  
-  // Get user's company from user attributes
-  //const company = event.requestContext.authorizer.claims['custom:Company'];
   
   const client = new IoTClient({ region });
 
@@ -32,31 +26,91 @@ export const handler = async (event, context, callback) => {
   const command = new CreateProvisioningClaimCommand({ templateName });
 
   try {
+
       const data = await client.send(command);
-      return {
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: JSON.stringify({
-              certificateId: data.certificateId,
-              certificatePem: data.certificatePem,
-              keyPair: data.keyPair,
-              expiration: data.expiration
-          }),
-      };
-  } catch (err) {
-      return {
-          statusCode: 500,
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          },
-          body: JSON.stringify({
-              code: 0, // see enum above
-              message: "Failed to create provisioning claim",
-              description: err.message
-          }),
-      };
+
+      return successApiResponse(stage, {
+        message: 'Successfully created provisioning claim',
+        certificateId: data.certificateId,
+        certificatePem: data.certificatePem,
+        keyPair: data.keyPair,
+        expiration: data.expiration
+      });
+
+  } catch (error) {
+
+    if (error.name === 'InternalFailureException') {
+
+      return errorApiResponse(
+        stage,
+        500,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'An internal failure prevented the provisionig claim from being created',
+        error.message
+      );
+
+    } else if (error.name === 'InvalidRequestException') {
+
+      return errorApiResponse(
+        stage,
+        400,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'An invalid request prevented the provisionig claim from being created',
+        error.message
+      );
+
+    } else if (error.name === 'ResourceNotFoundException') { 
+
+      return errorApiResponse(
+        stage,
+        404,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'A non existing required resource prevented the provisionig claim from being created',
+        error.message
+      );
+
+    } else if (error.name === 'ServiceUnavailableException') {
+
+      return errorApiResponse(
+        stage,
+        503,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'Service unavailability prevented the provisionig claim from being created',
+        error.message
+      );
+
+    } else if (error.name === 'ThrottlingException') {
+
+      return errorApiResponse(
+        stage,
+        400,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'Service throttling prevented the provisionig claim from being created',
+        error.message
+      );
+
+    } else if (error.name === 'UnauthorizedException') {
+      
+      return errorApiResponse(
+        stage,
+        401,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'Unauthorized access prevented the provisionig claim from being created',
+        error.message
+      );
+
+    } else {
+
+      return errorApiResponse(
+        stage,
+        500,
+        'FAILED_PROVISIONING_CLAIM_CREATION',
+        'An unexpected error prevented the provisionig claim from being created',
+        error.message
+      );
+
+    }
+
   }
 
 };
